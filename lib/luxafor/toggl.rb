ffa907759da3dfaad9ad0da4ff29850bfb822519
Gabriel Fortuna <gabriel@zero-one.io>
2020-04-11 15:58:48 +0200

    feat: add some init methods

--- lib/luxafor/toggl.rb
+++ lib/luxafor/toggl.rb
@@ -3,6 +3,40 @@ require "luxafor/toggl/version"
 module Luxafor
   module Toggl
     class Error < StandardError; end
-    # Your code goes here...
+
+    extend self
+
+    EXPECTED_ERRORS = [
+      # Filelock::ExecTimeout,
+      # Filelock::WaitTimeout,
+      # KeyError,
+      # Oj::Error,
+      # Oj::ParseError,
+      # Oj::DepthError,
+      # Oj::LoadError,
+      # ValueSemantics::MissingAttributes,
+    ].freeze
+
+    attr_writer :toggl_key, :luxafor_key, :state_file
+
+    def toggl_key
+      @toggl_key || ENV.fetch('TOGGL_KEY')
+    end
+
+    def luxafor_key
+      @luxafor_key || ENV.fetch('LUXAFOR_KEY')
+    end
+
+    def state_file
+      @state ||= ENV.fetch("LUXAFOR_TOGGL_STATE_FILE", "/tmp/#{Etc.getpwuid(Process.euid).name}-luxafor-toggle-state.json")
+    end
+
+    def toggl_client
+      @toggl_client ||= TogglV8::API.new(toggle_key)
+    end
+
+    def luxafor_client
+      @luxafor_client ||= Luxafor::Client.new(luxafor_key)
+    end
   end
 end

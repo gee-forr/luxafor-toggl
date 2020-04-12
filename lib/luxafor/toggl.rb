121b59f91d115189b20deb169d047d8d6fc60461
Gabriel Fortuna <gabriel@zero-one.io>
2020-04-12 18:42:53 +0200

    style: adjust rubocop style issues

--- lib/luxafor/toggl.rb
+++ lib/luxafor/toggl.rb
@@ -1,18 +1,18 @@
-require "date"
-require "value_semantics"
 # require "awesome_print"
+require 'date'
+require 'value_semantics'
 
 require 'luxafor'
 require 'togglv8'
 
-require "luxafor/toggl/version"
-require "luxafor/toggl/client"
-require "luxafor/toggl/state"
+require 'luxafor/toggl/version'
+require 'luxafor/toggl/client'
+require 'luxafor/toggl/state'
 
 # As of Apr 2020, because of a 1/2 removed awesome print dep in the gem which hasnt been released.
 # See https://github.com/kanet77/togglv8/issues/17
 class Logger
-  def ap(*args, **kwargs, &block)
+  def ap(*_args, **_kwargs, &_block)
     true
   end
 end
@@ -45,11 +45,11 @@ module Luxafor
     end
 
     def state_file
-      @state ||= ENV.fetch("LUXAFOR_TOGGL_STATE_FILE", "/tmp/#{Etc.getpwuid(Process.euid).name}-luxafor-toggl-state.json")
+      @state ||= ENV.fetch('LUXAFOR_TOGGL_STATE_FILE', "/tmp/#{Etc.getpwuid(Process.euid).name}-luxafor-toggl-state.json")
     end
 
     def idle_time
-      @idle_time ||= ENV.fetch("LUXAFOR_TOGGL_IDLE_TIME", 60 * 60 * 2).to_i # Default is 2 hours before you're idle
+      @idle_time ||= ENV.fetch('LUXAFOR_TOGGL_IDLE_TIME', 60 * 60 * 2).to_i # Default is 2 hours before you're idle
     end
 
     def toggl_client
